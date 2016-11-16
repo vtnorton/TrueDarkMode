93f8e10e7ae82545b8ffd19f441f111500e975e9
Vítor Norton <contato@vtnorton.com>
2016-11-16 15:28:18 -0200

    First commit!

---
+++ src/js/www/web.whatsapp.com.js
@@ -0,0 +1,12 @@
+﻿$(document).ready( function(){
+    //var background = "#fff !important";
+    alert("oi77");
+    //document.getElementsByClassName("icon").style.background = background;
+    //document.getElementsByClassName("icon-s").style.background =  background;
+    //document.getElementsByClassName("icon-l").style.background = background;
+    //document.getElementsByClassName("icon-xl").style.background = background;
+});
+
+//$(".pane-list-header").load(function () {
+//    alert("oi");
+//});
